80c67aed6d81edaaa93a788e1e37ab5f4d26ed5a
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-26 20:34:10 +0800

    Ternary Operator

---
+++ methods_one/ternary_operator.rb
@@ -0,0 +1,8 @@
+def check_pokemon(pokemon)
+    pokemon == "Charizard" ? "Fireball!" : "That is not Charizard!"
+end
+
+p check_pokemon("Pikachu")
+p check_pokemon("Squirtle")
+p check_pokemon("Onix")
+p check_pokemon("Charizard")
