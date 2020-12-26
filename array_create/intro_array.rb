78249b2613451963fc7a645fcc7a86712a649caa
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-26 21:54:32 +0800

    Create An Array with Bracket Syntax

--- array_create/intro_array.rb
+++ array_create/intro_array.rb
@@ -1,5 +1,17 @@
-p []
-p [].class
-
-numbers = []
+numbers = [4, 8, 15, 16, 23, 42]
 p numbers
+
+toy = ["Teddy bear", "Super Soaker", "board game"]
+p toy
+
+things = [4, true, "Hello", 10.99]
+p things
+
+registrations = [true, true, false, true, false]
+p registrations
+
+students = [["Boris", 25, true],
+            ["Sally", 23, true],
+            ["Ingrid", 31]]
+
+p students
