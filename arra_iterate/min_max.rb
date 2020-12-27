f4aaa87a021e9743a36cc7c74cb37bd8286c46e3
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 16:30:05 +0800

    The .max and .min Methods on an Array

---
+++ arra_iterate/min_max.rb
@@ -0,0 +1,20 @@
+def custom_max(arr)
+    return nil if arr.empty?
+    max = arr[0]
+    arr.each do |value|
+        max = value if value > max
+    end
+    max
+end
+
+def custom_min(arr)
+    return nil if arr.empty?
+    min = arr[0]
+    arr.each { |value| min = value if value < min }
+    min
+end
+
+numbers = [3, 9, 5, 7, 10, 1]
+
+p custom_max(numbers)
+p custom_min(numbers)
