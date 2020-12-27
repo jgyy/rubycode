2fd3010fee86d60580e6b72813248dd3388dc629
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 16:54:55 +0800

    The .partition Method on an Array

---
+++ arra_iterate/partition_method.rb
@@ -0,0 +1,13 @@
+foods = [
+    "Steak",
+    "Vegetables",
+    "Steak Burger",
+    "Kale",
+    "Tofu",
+    "Tuna Steaks"
+]
+
+ good, bad = foods.partition { |food| food.include?("Steak") }
+
+ p good
+ p bad
