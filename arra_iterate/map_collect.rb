c65380932057455f08d41accd130ca7f46265310
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 15:27:22 +0800

    The .map and .collect Methods on an Array

---
+++ arra_iterate/map_collect.rb
@@ -0,0 +1,12 @@
+numbers = [3, 8, 11, 15, 89]
+
+# Writes a cubes method that accepts an array
+# and returns a new array. The new array will
+# have all the values from the original one cubed.
+
+def cubes(array)
+    array.map { |number| number ** 3 }
+end
+
+p cubes(numbers)
+p cubes([3, 5, 8, 13, 17, 1000])
