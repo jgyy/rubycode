6bf9cf11149853c9fa1cc2737d7a7ccf9b5615c2
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 15:45:18 +0800

    The break Keyword

--- arra_iterate/break_keyword.rb
+++ arra_iterate/break_keyword.rb
@@ -1,25 +1,10 @@
-prizes = [
-    "Pyrite",
-    "Pyrite",
-    "Pyrite",
-    "Pyrite",
-    "Pyrite",
-    "Gold",
-    "Pyrite",
-    "Pyrite",
-    "Pyrite",
-    "Pyrite",
-    "Pyrite"
-]
+numbers = [1, 2, 3, "Hello", 5, 6, 7, 8]
 
-i = 0
-while i < prizes.length
-    current = prizes[i]
-    if current == "Gold"
-        puts "Yay! Found gold!"
-        break
+numbers.each do |num|
+    if num.is_a?(Fixnum)
+        puts "The square of #{num} is #{num ** 2}"
     else
-        puts "#{current} is not gold!"
+        puts "That's not a valid number, I'm done with this nonsense!"
+        break
     end
-    i += 1
 end
