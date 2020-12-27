a40e36623423c401bfaa2dad063c83aa42a5cb30
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 11:52:25 +0800

    Overwrite One or More Array Elements

---
+++ array_create/array_elements.rb
@@ -0,0 +1,20 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+p fruits
+
+fruits[4] = "Raspberry"
+p fruits
+
+fruits[5] = "Strawberry"
+p fruits
+
+fruits[10] = "Kiwi"
+p fruits
+
+fruits[3, 2] = ["Canteloupe", "Dragonfruit"]
+p fruits
+
+fruits[0..2] = ["Blackberry", "Orange", "Pears"]
+p fruits
+
+fruits[0..3] = ["Blah"]
+p fruits
