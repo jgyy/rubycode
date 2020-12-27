9c40304c2fe55aacd4ae423698dd06f6f75e2931
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 16:00:16 +0800

    The .sort Method on an Array

---
+++ arra_iterate/sort_method.rb
@@ -0,0 +1,10 @@
+numbers = [5, 13, 1, -2, 8]
+words = ["Caterpillar", "Kangaroo", "Apple", "Zebra"]
+p numbers
+p words
+
+numbers = numbers.sort.reverse
+words = words.sort.reverse
+
+p numbers
+p words
