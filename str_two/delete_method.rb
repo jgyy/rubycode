7d58cbabe9412f23fb943fc09ab7dc0e2e5a3ac5
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-27 18:15:45 +0800

    The .delete Method on a String

---
+++ str_two/delete_method.rb
@@ -0,0 +1,7 @@
+def custom_delete(string, delete_characters)
+    new_string = ""
+    string.each_char { |char| new_string << char unless delete_characters.include?(char) }
+    new_string
+end
+
+p custom_delete("Hello worlds", "s")
