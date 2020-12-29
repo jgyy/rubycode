73d629645a502d4f46d63ae10853f7928382d404
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-29 14:20:48 +0800

    Convert Hash to Array and Vice Versa

---
+++ hashe_two/hash_array.rb
@@ -0,0 +1,10 @@
+power_rangers = [
+    [:red, "Jason"],
+    [:black, "Zack"],
+    [:blue, "Billy"],
+    [:yellow, "Trini"],
+    [:pink, "Kimberly"]
+]
+
+puts power_rangers.to_h
+puts power_rangers.to_h.class
