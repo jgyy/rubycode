7f09934123ab6cd083d83e71daf9fdaca2f938f2
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-29 11:01:14 +0800

    The .inject and .reduce Methods on an

---
+++ arr_bonus/inject_reduce.rb
@@ -0,0 +1,7 @@
+result = [3, 4, 5, 6, 7].reduce(1) do |previous, current|
+    puts "The previous value is #{previous}"
+    puts "The current value is #{current}"
+    previous * current
+end
+
+p result
