1135e3b0886992014e458edf69745f444c256f2e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-29 13:31:12 +0800

    The .length and .empty? Methods on a

---
+++ hashes_one/length_empty.rb
@@ -0,0 +1,8 @@
+shopping_list = {bananas: 5, orange: 10, carrots: 3, crackers: 15}
+vegan_shopping_list = {}
+
+p shopping_list.length
+p vegan_shopping_list.length
+
+p shopping_list.empty?
+p vegan_shopping_list.empty?
