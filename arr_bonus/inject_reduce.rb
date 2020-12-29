609ef4377cc476da2420909ebfbb96be108e0744
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-29 11:07:45 +0800

    The .flatten Method on an Array

--- arr_bonus/inject_reduce.rb
+++ arr_bonus/inject_reduce.rb
@@ -1,4 +1,4 @@
-result = [3, 4, 5, 6, 7].reduce(1) do |previous, current|
+result = [3, 4, 5, 6, 7].inject(1) do |previous, current|
     puts "The previous value is #{previous}"
     puts "The current value is #{current}"
     previous * current
