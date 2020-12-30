af056306786292987db8a2b6b14fc54abf7ae75c
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-30 13:25:40 +0800

    The Comparable Methods on a Time Object

---
+++ time_object/comparable.rb
@@ -0,0 +1,8 @@
+birthday = Time.new(2016, 4, 12)
+summer = Time.new(2016, 6, 21)
+independence_day = Time.new(2016, 7, 4)
+winter = Time.new(2016, 12, 21)
+
+puts birthday.between?(Time.new(2016, 1, 1), winter)
+p summer
+p independence_day
