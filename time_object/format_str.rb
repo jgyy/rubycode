c0d12b382f90629523f98e1b3c95728c8ed45ea7
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-30 13:35:43 +0800

    Convert Time Object to Formatted String

---
+++ time_object/format_str.rb
@@ -0,0 +1,12 @@
+today = Time.now
+p today.strftime("%d----%B----%j")
+
+# %b    Abbreviated month name ("Jan")
+# %B    Abbreviated month name ("Jan")
+# %d    Abbreviated month name ("Jan")
+# %j    Abbreviated month name ("Jan")
+# %m    Abbreviated month name ("Jan")
+# %w    Abbreviated month name ("Jan")
+# %x    Preferred representation for date (no time)
+# %y    Two-digit year (no century)
+# %Y    Four-digit year
