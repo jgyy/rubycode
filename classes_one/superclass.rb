2b3cb8664a06ac64aaa91866f0c8c3cf570ee8d6
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-12-31 15:08:14 +0800

    The .superclass and .ancestors Methods on

---
+++ classes_one/superclass.rb
@@ -0,0 +1,10 @@
+p 5.class
+p 5.class.superclass
+p 5.class.superclass.superclass
+p 5.class.superclass.superclass.superclass
+
+puts
+
+p 5.class.ancestors
+p 3.14.class.ancestors
+p [1, 2, 3].class.ancestors
